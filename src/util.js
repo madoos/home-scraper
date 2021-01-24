a8f489aa73398bf56ae80c58cb2ab14e4819055a
rerades <rerades@siete3.com>
2021-01-24 17:24:29 +0100

    adding test

--- src/util.js
+++ src/util.js
@@ -1,15 +1,19 @@
 const { fromPromise } = require('crocks/Async');
 const { curryN } = require('ramda');
+const curry = require('crocks/helpers/curry');
 
 // log :: a -> ()
-const log = (x) => console.log( x['inspect'] ? x.inspect() : x);
+const log = x => console.log(x['inspect'] ? x.inspect() : x);
+// labelLog :: string -> a -> ()
+const labelLog = curry(label => x => console.log(`${label}:`, log(x)));
 
 // toAsync :: x, ...z -> Promise Error a -> (x -> ... -> z -> Async Error a)
-const toAsync = (f) => curryN(f.length, fromPromise(f));
+const toAsync = f => curryN(f.length, fromPromise(f));
 
-// toAsync 
+// toAsync
 
 module.exports = {
-    log,
-    toAsync
+  log,
+  labelLog,
+  toAsync,
 };
